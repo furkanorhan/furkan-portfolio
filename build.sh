54d3090a614d2120130b761aa0a5c48241b95c88
furkanorhan <furkan6062014@hotmail.com>
2024-12-29 17:13:25 +0300

    Update build configuration

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-#!/usr/bin/env bash
-# exit on error
+#!/bin/bash
 set -o errexit
 
+echo "Installing dependencies..."
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "Running migrations..."
 python manage.py migrate 
